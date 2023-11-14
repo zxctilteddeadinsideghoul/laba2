8dfe7e190025db2cddab9a7950fc9b5199f31584
sadtowa <toni.afanasev.0505@mail.ru>
2023-11-14 20:12:50 +0500

    separate number and operations from input string

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Program{
+    public class ExtendedOperation
+    {
+        public string Operation;
+        public int Priority;
+    }
+    public static readonly string[] numberSeparators = new string[] {" ", "+", "-", "/", "*", "(", ")"};
+    public static readonly string[] operationSeparators = new string[] {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+    
+    static List<string> Separate(string expression, string[] Separator)
+    {
+        List<string> result = new List<string>();
+        
+        foreach(string elem in expression.Split(Separator, StringSplitOptions.RemoveEmptyEntries))
+        {
+            result.Add(elem);
+        }
+        
+        return result;
+    }
+    
+    static void Main()
+    {
+        
+    }
+
+
+}
